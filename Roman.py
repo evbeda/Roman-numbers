dbfe5b4a1e66dca8e31d6fb00a1ed068eb156015
Cmoyano <cristianmoyano.mza@gmail.com>
2018-02-26 10:04:25 -0300

    test 1 a 4

--- Roman.py
+++ Roman.py
@@ -2,15 +2,37 @@ def roman(numero):
 	if numero < 1:
 		return None
 
-	numeros = {'1': 'I', '5' : 'V', '10': 'X', '50': 'L'}
+	numeros = {'1': 'I', '4':'IV', '5' : 'V', '9':'IX', '10': 'X',
+	 '40':'XL','50': 'L'}
+	
 
-	if numero == 1 : 
-		return 'I' 
+	# if numero == 1 : 
+	# 	return 'I' 
+	# elif numero == 2 :
+	# 	return 'X' 
 
-	elif numero == 10 :
-		return 'X' 
-	elif numero == 845 :
-		return 'DCCCXLV'
-#reference key(numeros)
+	# elif numero == 10 :
+	# 	return 'X' 
+	# elif numero == 845 :
+	# 	return 'DCCCXLV'
+	# for x in numeros(1,10):
+	# 	pass
+
+
+	#for key, value in numeros.iteritems():
+		#if  numero == 1:
+		#	return 'I'
+	if numero >= 1 and numero < 4:
+		return numeros[str(1)] * numero
+
+	if numero == 4:
+		return numeros[str(4)]
+
+	#if numero > 5 and numero < 9:
+
+
+
+
+
 
 
