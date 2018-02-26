bcbf379a0d2e95ee36584e6161ea4a8a5f5c0711
manu <manupxndx@gmail.com>
2018-02-26 14:48:50 -0300

    modify Romany.py

--- Roman.py
+++ Roman.py
@@ -7,21 +7,12 @@ def roman(numero):
 
     if not isinstance(numero, int):
         return "Only int numbers"
+
     numeros = {
-        1000: 'M',
-        900: 'CM',
-        500: 'D',
-        400 :'CD',
-        100 :'C',
-        90 : 'XC',
-        50 : 'L',
-        40 : 'XL',
-        10: 'X',
-        9 : 'IX',
-        5 : 'V',
-        4 : 'IV',
-        1 : 'I',
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L',
+        40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I',
     }
+
     roman = ''
 
     for key in sorted(numeros.keys(), reverse=True):
